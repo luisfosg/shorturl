bd0bf569307258d968776393d61835e0946ff0b7
Luis Osorio <luisfernandogarciaosorio@gmail.com>
2021-03-29 20:09:13 -0500

    dividiendo metodos de guardado de url

--- src/controllers/data-controller.js
+++ src/controllers/data-controller.js
@@ -1,14 +1,6 @@
 import * as user from '../libs/infoUser';
-/** Metodo POST para guardar URLs
- * @type {function}
- * @param {Object} req - "request" de la ruta
- * @param {Object} res - "response" de la ruta
- * @param {function} res.status - función para enviar un estado http con json
-*/
 
-export const sendUrl = async ( req, res ) => {
-	if ( !req.urlSend ) return user.userInfo( req, res );
-
+const withUser = async ( req, res ) => {
 	const {
 		nick,
 		password,
@@ -21,6 +13,36 @@ export const sendUrl = async ( req, res ) => {
 	res.status( 200 ).json( {
 		nick, password, shortUrl, destinationUrl, passwordUrl, views, user: req.user
 	} );
+};
+
+const withoutUser = async ( req, res ) => {
+	const {
+		shortUrl,
+		destinationUrl,
+		passwordUrl,
+		views
+	} = req.body;
+
+	res.status( 200 ).json( {
+		shortUrl, destinationUrl, passwordUrl, views
+	} );
+};
+
+/** Metodo POST para guardar URLs
+ * @type {function}
+ * @param {Object} req - "request" de la ruta
+ * @param {Object} res - "response" de la ruta
+ * @param {function} res.status - función para enviar un estado http con json
+*/
+
+export const sendUrl = async ( req, res ) => {
+	if ( !req.urlSend ) return user.userInfo( req, res );
+
+	if ( !req.register ) {
+		withoutUser( req, res );
+	} else {
+		withUser( req, res );
+	}
 };
 
 /** Metodo POST para guardar URLs
