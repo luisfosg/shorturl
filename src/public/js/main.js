2ab0c530b99efcaed206954852fadc8303486a61
Luis Osorio <luisfernandogarciaosorio@gmail.com>
2021-04-04 20:37:46 -0500

    funcion para copiar ruta de la Url

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -14,6 +14,8 @@ function hideOrShowPassword() {
 	}
 };
 
+/* ======================================================================== */
+
 const closed = document.getElementById( 'closee' );
 
 if ( closed ) {
@@ -24,6 +26,8 @@ function closeAlert() {
 	const alerta = document.getElementById( 'alerta' );
 	alerta.remove();
 }
+
+/* ======================================================================== */
 
 const buttonDelete = document.getElementsByClassName( 'alerta' );
 if ( buttonDelete ) {
@@ -48,5 +52,31 @@ function viewAlert( id ) {
 		if (result.isDenied) {
 			window.location.href = `/delete/${ id }`;
 		}
+	})
+}
+/* ======================================================================== */
+
+const copyText = document.getElementsByClassName( 'copiarTexto' );
+if ( copyText ) {
+	for (let i = 0; i < copyText.length; i++) {
+		const text = copyText[i].getAttribute("value");
+		const title = copyText[i].getAttribute("text");
+		copyText[i].addEventListener( 'click', function(){  copyTheText( title, text ) } );
+	}
+}
+
+function copyTheText(title, text) {
+	var aux = document.createElement("input");
+	aux.setAttribute("value", text);
+	document.body.appendChild(aux);
+	aux.select();
+	document.execCommand("copy");
+	document.body.removeChild(aux);
+
+	Swal.fire({
+		icon: 'success',
+		title: `${title} copiado correctamente`,
+		showConfirmButton: false,
+		timer: 1500
 	})
 }
