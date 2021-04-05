c53f3c5ffc0cf1006e498a7a06f1cb320453a899
Luis Osorio <luisfernandogarciaosorio@gmail.com>
2021-04-04 19:56:13 -0500

    agregando boton para eliminar y editar urls

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -24,3 +24,29 @@ function closeAlert() {
 	const alerta = document.getElementById( 'alerta' );
 	alerta.remove();
 }
+
+const buttonDelete = document.getElementsByClassName( 'alerta' );
+if ( buttonDelete ) {
+	for (let i = 0; i < buttonDelete.length; i++) {
+		const id = buttonDelete[i].getAttribute("value");
+		buttonDelete[i].addEventListener( 'click', function(){ viewAlert( id ) } );
+	}
+}
+
+function viewAlert( id ) {
+	Swal.fire({
+		title: 'Eliminar',
+		icon: 'warning',
+		html:
+		  '¿ Desea Eliminar la Url Seleccionada ?',
+		showDenyButton: true,
+		showCancelButton: true,
+		showConfirmButton: false,
+		denyButtonText: `Si, Eliminar`,
+		cancelButtonText: `Cancelar`
+	}).then((result) => {
+		if (result.isDenied) {
+			window.location.href = `/delete/${ id }`;
+		}
+	})
+}
